10cb2fef9a96ce6871bfee010ca4e1a2c038fe69
Oohatagakuto <31167@ojs.ac.jp>
2020-09-24 11:59:00 +0900

    List<> ~p90

--- Chapter3/Program.cs
+++ Chapter3/Program.cs
@@ -8,46 +8,23 @@ namespace Chapter3
 {
     class Program
     {
-        //デリゲートの宣言(int型の引数を受け取ってbool型を返すメソッド)
-        public delegate bool Judgement(int value);
+
         static void Main(string[] args)
         {
-            var numbers = new[] { 5, 3, 9, 6, 7, 5, 8, 1, 0, 5, 10, 4 };
-
-            //匿名メソッドを使用した例
-            //int count = Count(numbers,delegate(int n){ return (n >= 5);}) ;
-            int count = Count(numbers,  n => 5 <=n && n <= 8);
-            Console.WriteLine(count);
-        }
-
-        //nが偶数かどうか調べる
-       // public static bool IsEven(int n)
-       //{
-       //     return (n >= 5);
-
-            //return (n % 2 == 0);
-
-            //if (n % 2 ==0)
-            //{
-            //    return true;
-            //}
-            //else
-            //{
-            //    return false;
-            //}
-       // }
-
-
-        public static int Count(int[] numbers, Judgement judge)
-        {
-           
-            int count = 0;
-            foreach(var n in numbers)
+            var list = new List<string>
             {
-                if (judge(n)==true)
-                    count++;
-            }
-            return count;
+                "Tokyo","New Delhi","Bangkok","London","Paris","Berlin","Canberra","HongKong",
+            };
+
+            //list.ForEach(s => Console.WriteLine(s));
+
+             list.ConvertAll(s => s.ToUpper()).ForEach(s=>Console.WriteLine(s));
+            //foreach (var item in name)
+            //{
+            //    Console.WriteLine(item);
+            //}
+           
+
         }
     }
 }
