83d1709e5e524ff2f63fb903befb369241ca2a94
Ehara Suzuna <31155@ojs.ac.jp>
2020-09-25 15:43:39 +0900

    3.1-2

--- Chapter3/Program.cs
+++ Chapter3/Program.cs
@@ -13,16 +13,7 @@ namespace Chapter3
         {
             var numbers = new List<int> { 12, 87, 94, 14, 53, 20, 40, 35, 76, 91, 31, 17, 48 };
 
-            var exists = numbers.Exists(s => s%8 == 0 || s%9==0 );
-
-            if (exists == true)
-            {
-                Console.WriteLine("あったよ");
-            }
-            else
-            {
-                Console.WriteLine("なかったよ");
-            }
+            numbers.ForEach(s => Console.WriteLine(s / 2.0));
             
 
         }
