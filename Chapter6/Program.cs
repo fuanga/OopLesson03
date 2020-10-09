6df27703547d1d7a7a885f11ec20f56abbcccdc2
Ehara Suzuna <31155@ojs.ac.jp>
2020-10-09 15:08:59 +0900

    6-1 終了

--- Chapter6/Program.cs
+++ Chapter6/Program.cs
@@ -12,52 +12,63 @@ namespace Chapter6
     {
         static void Main(string[] args)
         {
-            //整数の例
-            var numbers = new List<int> { 19, 17, 15, 24, 12, 25, 14, 20, 12, 28, 19, 30, 24 };
+            var numbers = new int[] { 5, 10, 17, 9, 3, 21, 10, 40, 21, 3, 35 };
+            //6.1-1
+            Console.WriteLine("______________6.1-1______________");
+            Console.WriteLine(numbers.Max());
 
-            //var strings = numbers.Select(n => n.ToString("0000")).ToArray();
-            //foreach (var str in strings)
-            //{
-            //    Console.Write(str + " ");
-            //}
-            
-
-            numbers.Distinct().Select(n => n.ToString("0000")).ToList().ForEach(S => Console.Write(S + " "));
-
-            Console.WriteLine();//改行
-
-            //並べ替え
-            var sortedNumbers = numbers.OrderBy(n => n);
-            foreach (var nums in sortedNumbers)
-            {
-                Console.Write(nums + " ");
-            }
-
-            //文字列の例
-            var words = new List<string> { "Microsoft", "Apple", "Google", "Oracle", "Facebook", };
-
-            var loewer = words.Select(name => name.ToLower()).ToArray();
-
-            //オブジェクトの例
-            var books = Books.GetBooks();
-
-
-            //タイトルリスト
-            var tatles = books.Select(x => x.Title);
-
-            foreach (var title in tatles)
-            {
-                Console.Write(title + " ");
-            }
-
+            //6.1-2
             Console.WriteLine();
-            //ページ数の多い順に並べ替え（または金額の高い順）
-            var sortedBooks = books.OrderByDescending(n => n.Pages);
-            foreach (var item in sortedBooks)
+            Console.WriteLine("______________6.1-2______________");
+            int pos = numbers.Length - 2;
+            foreach(var number in numbers.Skip(pos))
             {
-                Console.Write(item.Title + " " + item.Pages);
+                Console.Write(number + " ");
             }
+            Console.WriteLine();
+            //6.1-3
+            Console.WriteLine();
+            Console.WriteLine("______________6.1-3______________");
+            var strNums = numbers.Select(n => n.ToString());
+            foreach (var str in strNums)
+            {
+                Console.Write(str + " ");
+            }
+
+
+            //6.1-4
+            Console.WriteLine();
+            Console.WriteLine("______________6.1-4______________");
+            var sorte = numbers.OrderBy(x => x);
+            foreach (var item in sorte.Take(3))
+            {
+                Console.Write(item + " ");
+            }
+            //6.1-5
+            Console.WriteLine();
+            Console.WriteLine("______________6.1-5______________");
+            Console.WriteLine( numbers.Distinct().Count(n => n > 10));
+
+            //6.2-1
+
+
+
+
+
+            var books = new List<Book> {
+  
+                new Book { Title = "C#プログラミングの新常識", Price = 3800, Pages = 378 },
+                new Book { Title = "ラムダ式とLINQの極意", Price = 2500, Pages = 312 },
+                new Book { Title = "ワンダフル・C#ライフ", Price = 2900, Pages = 385 },
+                new Book { Title = "一人で学ぶ並列処理プログラミング", Price = 4800, Pages = 464 },
+                new Book { Title = "フレーズで覚えるC#入門", Price = 5300, Pages = 604 },
+                new Book { Title = "私でも分かったASP.NET MVC", Price = 3200, Pages = 453 },
+                new Book { Title = "楽しいC#プログラミング教室", Price = 2540, Pages = 348 },
+
+            };
+
         }
+
 
     }
 }
